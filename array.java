a274bda1868751536dfa7827c1fe7e6925a812e9
praveen280405 <156937418+praveen280405@users.noreply.github.com>
2025-07-08 01:31:57 +0530

    Create array.java

---
+++ array.java
@@ -0,0 +1,40 @@
+// largest element inn an array:
+class Solution {
+    public int largestElement(int[] nums) {
+    int large = nums[0];
+    for(int i=1;i<nums.length;i++){
+        if(nums[i] > large){
+            large = nums[i];
+        }
+    }
+    return large;
+}
+}
+// Second largest element in an array:
+class Solution {
+    int thirdLargest(int arr[]) {
+        if (arr.length<3){
+            return -1;
+        }
+        int first = Integer.MIN_VALUE;
+        int sec = Integer.MIN_VALUE;
+        int third = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            int num = arr[i];
+            if(arr[i]>first){
+                third = sec;
+                sec = first;
+                first = num;
+            }
+            else if(arr[i]>= sec){
+                third = sec;
+                sec = num;
+            }
+            else if(arr[i]>=third){
+                third = num;
+            }
+        }
+        return third;
+    }
+}
