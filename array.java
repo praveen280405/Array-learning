ac40f9697d0c5874b4dbbd6011989baa3450f89b
praveen280405 <156937418+praveen280405@users.noreply.github.com>
2025-07-08 22:20:38 +0530

    Update array.java

--- array.java
+++ array.java
@@ -10,7 +10,36 @@ class Solution {
     return large;
 }
 }
-// Second largest element in an array:
+// second largest in array:
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        if(arr.length<2){
+            return -1;
+        }
+         int first = Integer.MIN_VALUE;
+        int sec = Integer.MIN_VALUE;
+        
+        for(int i=0;i<arr.length;i++){
+            int num = arr[i];
+            if(arr[i]>first){
+                sec = first;
+                first = num;
+            }
+            else if(arr[i]>= sec && arr[i]<first){
+                sec = num;
+            }
+          
+        }
+        if(sec == Integer.MIN_VALUE){
+            return -1;
+        }else{
+        return sec;
+            
+        }
+    }
+}
+// third largest element in an array:
 class Solution {
     int thirdLargest(int arr[]) {
         if (arr.length<3){
